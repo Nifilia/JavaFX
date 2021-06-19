ebf84a3871a95d9dbb7f97d9f183c9d153cb8762
Andero <andero-ilmar.kore@khk.ee>
2021-06-19 11:08:18 +0300

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args){
+        System.out.println("Launching Application");
+        Application.launch(args);
+    }
+    @Override
+    public void init() throws Exception{
+        System.out.println("Application inits");
+        super.init();
+    }
+    @Override
+    public void start(Stage stage){
+        System.out.println("Application starts");
+        stage.show();
+    }
+    @Override
+    public void stop() throws Exception{
+        System.out.println("Application stops");
+        super.stop();
+    }
+}
