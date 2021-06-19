fbd17b0bf07e3cab65be29ae3909dbabf840f153
Andero <andero-ilmar.kore@khk.ee>
2021-06-19 18:22:00 +0300

    Tööleht 4.3

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,6 +9,7 @@ import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -23,48 +24,30 @@ import java.util.concurrent.Flow;
 
 public class Main extends Application {
 
-    CheckBox java;
-    CheckBox javascript;
-    CheckBox csharp;
-    Label selectedLangs;
-
     public static void main(String[] args){
         Application.launch(args);
     }
 
     @Override
     public void start(Stage stage) {
+        ToggleButton lightBtn = new ToggleButton("Light");
+        Label stateLbl = new Label();
+        lightBtn.setOnAction(event -> {
+            if(lightBtn.isSelected()){
+                stateLbl.setText("Light on!");
+            }
+            else{
+                stateLbl.setText("Light off!");
+            }
+        });
 
-        selectedLangs = new Label("No languages selected");
-
-        java = new CheckBox("Java");
-        javascript = new CheckBox("JavaScript");
-        csharp = new CheckBox("C#");
-
-        java.setOnAction(event -> show());
-        javascript.setOnAction(event -> show());
-        csharp.setOnAction(event -> show());
-
-        FlowPane root = new FlowPane(Orientation.VERTICAL, 0, 10);
-        root.getChildren().addAll(java, javascript, csharp, selectedLangs);
-        root.setPadding(new Insets(0, 0, 0, 10));
-
+        FlowPane root = new FlowPane(10, 10);
+        root.getChildren().addAll(lightBtn, stateLbl);
+        root.setPadding(new Insets(10));
         Scene scene = new Scene(root, 250, 200);
+
         stage.setScene(scene);
         stage.setTitle("Hello JavaFX");
         stage.show();
-    }
-
-    private void show(){
-        String allSelected = "";
-
-        if(java.isSelected()) allSelected += "Java ";
-        if(javascript.isSelected()) allSelected += "JavaScript ";
-        if(csharp.isSelected()) allSelected += "C# ";
-
-        if(allSelected.equals(""))
-            selectedLangs.setText("No languages selected");
-        else
-            selectedLangs.setText(allSelected);
     }
 }
