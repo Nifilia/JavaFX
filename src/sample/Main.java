48d496a4e32696f6de8021e7bff24d82e75407db
Andero <andero-ilmar.kore@khk.ee>
2021-06-19 13:04:57 +0300

    Tööleht 3.2.1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -23,14 +24,23 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
-        Label lbl = new Label("Hello");
-        Button btn = new Button("Click");
-        FlowPane root = new FlowPane();
-        root.getChildren().addAll(lbl, btn);
-        Scene scene = new Scene(root, 300, 200);
+        Label label1 = new Label("Label1");
+        Label label2 = new Label("Label2");
+        Label label3 = new Label("Label3");
+        Label label4 = new Label("Label4");
+        Label label5 = new Label("Label5");
+        Label label6 = new Label("Label6");
+        Label label7 = new Label("Label7");
+        Label label8 = new Label("Label8");
+        Label label9 = new Label("Label9");
+
+        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10, label1, label2, label3, label4, label5, label6, label7, label8, label9);
+        Scene scene = new Scene(root);
         stage.setScene(scene);
 
-        stage.setTitle("Layout in JavaFX");
+        stage.setTitle("FlowPane in JavaFX");
+        stage.setWidth(300);
+        stage.setHeight(200);
 
         stage.show();
     }
