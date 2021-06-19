5b07daa1ac293bd5e4f5081937944dbb254ec4b1
Andero <andero-ilmar.kore@khk.ee>
2021-06-19 13:36:17 +0300

    Tööleht 3.4.2

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,6 +9,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -27,16 +28,13 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
-        Label label1 = new Label("Label1");
-        Label label2 = new Label("Label2");
-        Label label3 = new Label("Label3");
-        Label label4 = new Label("Label4");
-        Label label5 = new Label("Label5");
-        Label label6 = new Label("Label6");
-        Label label7 = new Label("Label7");
+        Button btn = new Button("Hello");
+        VBox vbox = new VBox();
+        VBox.setVgrow(btn, Priority.ALWAYS);
+        btn.setMaxHeight(Double.MAX_VALUE);
+        vbox.getChildren().addAll(btn);
 
-        VBox root = new VBox(10, label1, label2, label3, label4, label5, label6, label7);
-        Scene scene = new Scene(root, 300, 150);
+        Scene scene = new Scene(vbox, 300, 150);
         stage.setScene(scene);
 
         stage.setTitle("VBox in JavaFX");
