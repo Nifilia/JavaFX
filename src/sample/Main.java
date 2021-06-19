1891a13d72bf957017bbe54c56c8e632383d21f8
Andero <andero-ilmar.kore@khk.ee>
2021-06-19 11:37:08 +0300

    Tööleht 1.3.1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,17 +1,29 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class Main extends Application {
+
     public static void main(String[] args){
         Application.launch(args);
     }
     @Override
     public void start(Stage stage){
+        Label label = new Label("Hello");
+        Button button = new Button("Button");
+        Group group = new Group(button);
+
+        FlowPane root = new FlowPane(label, group);
+        Scene scene = new Scene(root, 300, 150);
+        stage.setScene(scene);
+
         stage.setTitle("Hello JavaFX");
-        stage.setWidth(250);
-        stage.setHeight(150);
         stage.show();
     }
 }
