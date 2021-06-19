bb633330c0032bad74f2dd4aa954c508df0acae4
Andero <andero-ilmar.kore@khk.ee>
2021-06-19 18:28:11 +0300

    Tööleht 4.4

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,13 +8,12 @@ import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.fxml.FXMLLoader;
 
 import java.awt.*;
@@ -30,24 +29,32 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        ToggleButton lightBtn = new ToggleButton("Light");
-        Label stateLbl = new Label();
-        lightBtn.setOnAction(event -> {
-            if(lightBtn.isSelected()){
-                stateLbl.setText("Light on!");
-            }
-            else{
-                stateLbl.setText("Light off!");
-            }
+
+        Label selectedLbl = new Label();
+        Button selectedBtn = new Button("Select");
+
+        RadioButton javaBtn = new RadioButton("Java");
+        RadioButton jsBtn = new RadioButton("JavaScripts");
+        RadioButton csharpBtn = new RadioButton("C#");
+
+        ToggleGroup group = new ToggleGroup();
+
+        javaBtn.setToggleGroup(group);
+        jsBtn.setToggleGroup(group);
+        csharpBtn.setToggleGroup(group);
+
+        selectedBtn.setOnAction(event -> {
+            RadioButton selection = (RadioButton) group.getSelectedToggle();
+            selectedLbl.setText("Selected: " + selection.getText());
         });
 
-        FlowPane root = new FlowPane(10, 10);
-        root.getChildren().addAll(lightBtn, stateLbl);
+        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10);
+        root.getChildren().addAll(javaBtn, jsBtn, csharpBtn, selectedBtn, selectedLbl);
         root.setPadding(new Insets(10));
         Scene scene = new Scene(root, 250, 200);
 
         stage.setScene(scene);
-        stage.setTitle("Hello JavaFX");
+        stage.setTitle("RadioButtons in JavaFX");
         stage.show();
     }
 }
