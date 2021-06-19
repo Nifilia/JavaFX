efc8c86cf5ad61aad43dd2eff08793877e74da34
Andero <andero-ilmar.kore@khk.ee>
2021-06-19 11:40:30 +0300

    Tööleht 1.3.2

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -15,14 +16,11 @@ public class Main extends Application {
     }
     @Override
     public void start(Stage stage){
-        Label label = new Label("Hello");
-        Button button = new Button("Button");
-        Group group = new Group(button);
 
-        FlowPane root = new FlowPane(label, group);
-        Scene scene = new Scene(root, 300, 150);
+        Group root = new Group();
+        Scene scene = new Scene(root, 400, 150, Color.BLUE);
+
         stage.setScene(scene);
-
         stage.setTitle("Hello JavaFX");
         stage.show();
     }
